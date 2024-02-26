96ef26f9e4b97884d528c7ba31cdba30822e402f
max lundin <max.lundin@kellogg.northwestern.edu>
2024-02-26 06:12:22 +0000

    Making some progress. Need to proerly set up the new pages and the controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,21 @@
 class EntriesController < ApplicationController
   def new
+
+    @entry = Entry.new
+
   end
 
   def create
+
+      @entry = Entry.new
+      @entry["title"] = params["entry"]["title"] 
+      @entry["date"] = params["entry"]["date"]
+      @entry["body"] = params["entry"]["body"]
+    
+      #need date and title @entry["date"] = params["entry"]["image"]
+      # TODO: assign logged-in user as user that created the post
+      @entry.save
+      redirect_to "/entries"
+
   end
 end
